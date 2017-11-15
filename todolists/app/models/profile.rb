c0dc7a1cfff8ee7e783bab2951b63c9d0c0a9573
m-rwash <m.rwash93@yahoo.com>
2017-11-15 23:33:37 +0200

    adding relationships/associations also vlaidations to models

--- todolists/app/models/profile.rb
+++ todolists/app/models/profile.rb
@@ -1,2 +1,23 @@
 class Profile < ActiveRecord::Base
+  belongs_to :user
+  validate :first_or_last_name_null
+  validates :gender, inclusion: ["male", "female"]
+  validate :cant_name_sue
+
+  def self.get_all_profiles(min_year, max_year)
+  	Profile.where("birth_year BETWEEN :min AND :max", min: min_year, max: max_year).order(:birth_year)	
+  end
+
+  def cant_name_sue
+  	if gender == 'male' && first_name == 'Sue'
+  		errors.add(:base, "can't name a male Sue")
+	end
+  end
+
+ def first_or_last_name_null
+  	if first_name.nil? && last_name.nil?
+  		errors.add(:base, "either first_name or last_name must be present!")
+  	end
+  end
+
 end
--- todolists/db/seeds.rb
+++ todolists/db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.destroy_all
+Profile.destroy_all
+TodoList.destroy_all
+TodoItem.destroy_all
+
+today = Date.today
+due_date = today + 1.year
+
+User.create!([
+	{username: "Fiorina", password_digest: "123abc"},
+	{username: "Trump", password_digest: "abc123"},
+	{username: "Carson", password_digest: "aabb"},
+	{username: "Clinton", password_digest: "bbaa"}
+])
+
+Fiorina = User.find_by username: "Fiorina"
+Fiorina.create_profile gender: "female", birth_year: 1954,
+	first_name: "Carly", last_name: "Fiorina"
+
+Trump = User.find_by username: "Trump"
+Trump.create_profile gender: "male", birth_year: 1946,
+	first_name: "Donald", last_name: "Trump"
+
+Carson = User.find_by username: "Carson"
+Carson.create_profile gender: "male", birth_year: 1951,
+	first_name: "Ben", last_name: "Carson"
+
+Clinton = User.find_by username: "Clinton"
+Clinton.create_profile gender: "female", birth_year: 1947,
+	first_name: "Hillary", last_name: "Clinton"
+
+User.all.each do |user|
+	user.todo_lists.create!(list_name: user.username, list_due_date: due_date)
+end
+
+TodoList.all.each do |todo_list|
+	5.times do
+		todo_list.todo_items.create!(due_date: due_date, title: "Title", description: "Description")
+	end
+end
