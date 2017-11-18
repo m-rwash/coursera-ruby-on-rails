b3113fbe1527a00987211e6a77c57179e96ab847
m-rwash <m.rwash93@yahoo.com>
2017-11-19 00:37:53 +0200

    add nested resources, authentication and authorization

--- todolists/config/routes.rb
+++ todolists/config/routes.rb
@@ -1,10 +1,23 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'todo_lists#index'
 
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :todo_lists do
+    resources :todo_items
+  end
+
+  get "/login" => "sessions#new", as: "login"
+  delete "/logout" => "sessions#destroy", as: "logout"
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
--- todolists/db/seeds.rb
+++ todolists/db/seeds.rb
@@ -1,47 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-User.destroy_all
-Profile.destroy_all
-TodoList.destroy_all
-TodoItem.destroy_all
-
 today = Date.today
-due_date = today + 1.year
+two_days_ago = Date.today - 2.days
+three_days_ago = Date.today - 3.days
+dates = [today, two_days_ago, three_days_ago]
 
-User.create!([
-	{username: "Fiorina", password_digest: "123abc"},
-	{username: "Trump", password_digest: "abc123"},
-	{username: "Carson", password_digest: "aabb"},
-	{username: "Clinton", password_digest: "bbaa"}
-])
+User.destroy_all
+TodoList.destroy_all
 
-Fiorina = User.find_by username: "Fiorina"
-Fiorina.create_profile gender: "female", birth_year: 1954,
-	first_name: "Carly", last_name: "Fiorina"
+100.times { |index| TodoList.create! list_name: "List #{index}", list_due_date: dates.sample }
 
-Trump = User.find_by username: "Trump"
-Trump.create_profile gender: "male", birth_year: 1946,
-	first_name: "Donald", last_name: "Trump"
-
-Carson = User.find_by username: "Carson"
-Carson.create_profile gender: "male", birth_year: 1951,
-	first_name: "Ben", last_name: "Carson"
-
-Clinton = User.find_by username: "Clinton"
-Clinton.create_profile gender: "female", birth_year: 1947,
-	first_name: "Hillary", last_name: "Clinton"
-
-User.all.each do |user|
-	user.todo_lists.create!(list_name: user.username, list_due_date: due_date)
+TodoList.all.each do |list|
+  list.todo_items.create! [
+    { title: "Task 1", due_date: dates.sample, description: "very important task TEST", completed: false },
+    { title: "Task 2", due_date: dates.sample, description: "do something else TEST", completed: true},
+    { title: "Task 3", due_date: dates.sample, description: "learn Action Pack TEST", completed: true}
+  ]
 end
 
-TodoList.all.each do |todo_list|
-	5.times do
-		todo_list.todo_items.create!(due_date: due_date, title: "Title", description: "Description")
-	end
+users = User.create! [
+  { username: "jim", password: "abc123" },
+  { username: "rich", password: "123abc" }
+]
+
+TodoList.all.each do |list|
+  list.user = users.sample
+  list.save!
 end
